22d2dfcca74dad839e97ab465c16f8b25f484046
BakaUSPSB <cjs5@njit.edu>
2024-04-11 16:26:15 -0400

    redo

--- initdb/04_role_data.sql
+++ initdb/04_role_data.sql
@@ -1,4 +1,3 @@
 insert into role (role_id, role) values (1, 'admin');
 insert into role (role_id, role) values (2, 'manager');
 insert into role (role_id, role) values (3, 'technician');
-insert into role (role_id, role) values (4, 'customer'); 
