567afa4887b7ef248f2388b25306db7248329370
BakaUSPSB <cjs5@njit.edu>
2024-04-11 15:16:36 -0400

    setup for mock vehicle(s) data for customer, added role and user data to initdb

--- initdb/04_role_data.sql
+++ initdb/04_role_data.sql
@@ -1,3 +1,4 @@
 insert into role (role_id, role) values (1, 'admin');
 insert into role (role_id, role) values (2, 'manager');
 insert into role (role_id, role) values (3, 'technician');
+insert into role (role_id, role) values (4, 'customer'); 
