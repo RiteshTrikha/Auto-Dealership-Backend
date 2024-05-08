e9ec255a48594d418c36ae5b7182279a9589a641
RiteshTrikha <rt436@njit.edu>
2024-05-08 04:04:50 -0400

    updated to test google cloud removed DB startup from docker-compose.yml to test updated run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
