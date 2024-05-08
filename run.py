de089affecb862cd37fe78758a714fa47c2bab3a
BakaUSPSB <144304219+BakaUSPSB@users.noreply.github.com>
2024-05-08 01:52:45 -0400

    added run configs for google cloud

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
